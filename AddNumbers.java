b6bfbf1acf13cbafae0ce0d602b631f40168c2cc
Guido Asbun <gdrodrigo8@gmail.com>
2020-11-30 21:48:01 -0800

    AddNumbers class complete

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,5 +1,24 @@
-import
+import java.util.*;
 
 public class AddNumbers {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double sum = 0.0;
+        System.out.println("Enter four floating-point numbers: ");
+
+        try {
+
+            for(int i = 0; i < 4; i++){
+                sum += in.nextInt();
+            }
+            System.out.printf("The sum of all correctly entered numbers is: %.2f\n", sum);
+            
+        } catch (InputMismatchException e) {
+            System.out.println("You have an invalid entry!!");
+            System.out.printf("The sum of all correctly entered numbers is: %.2f\n", sum);
+        }
+
+        in.close();
+    }
 }
